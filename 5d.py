47e2d45c40d39d704f876f1e63757bdb2b3e1c79
Venkata Rao Dechiraju <101019414+veradec@users.noreply.github.com>
2026-04-13 15:22:00 +0530

    Create 5d.py

---
+++ 5d.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+plt.subplot(2, 3, 1)
+plt.plot(x, y, 'r')
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+plt.subplot(2, 3, 2)
+plt.plot(x, y, 'b--')
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+plt.subplot(2, 3, 3)
+plt.plot(x, y, 'g^')
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+plt.subplot(2, 3, 4)
+plt.plot(x, y, 'c')
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+plt.subplot(2, 3, 5)
+plt.plot(x, y, 'm--')
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+plt.subplot(2, 3, 6)
+plt.plot(x, y, 'y*')
+
+plt.show()
